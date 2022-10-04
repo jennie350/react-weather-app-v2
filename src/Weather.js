de235d0e39f491e073939f34f715f33092757d18
Jenny Watts <93653195+jennie350@users.noreply.github.com>
2022-10-04 21:36:11 +0800

    Added base content and basic css

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,60 @@
 import React from "react";
+import './Weather.css'
 
 export default function Weather(){
     return (
+        <div className="Container">
         <div className="Wrapper">
-            <h1>Weather app</h1>
             
+            <div className="row">
+                <div className="col-9">
+                <input 
+                type="search" 
+                placeholder="🔍 Type city and press Enter"
+                className="form-control"/>
         </div>
+                    <div className="col-3">
+                        <input  type="submit" value="Current location" className="btn btn-success"/>
+                         </div>
+                         
+                         </div>
+                         <div className="row">
+                            
+                                <h1>
+                                Singapore</h1>
+                                
+                            </div>
+                            <div className="row">
+                                <p className="Last-updated">Last updated: Friday 12:55</p>
+                            </div>
+                            <div className="row">
+                                <div className="col-6">
+                                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny"/>
+                                </div>
+                                <div className="col-3">
+                                    <h2>27°C</h2>
+                                    <p>Feels like 25°C</p>
+                                </div>
+                                <div className="col-3">
+                                    <ul>
+                                        <li>
+                                            Sunny
+                                        </li>
+                                        <li>
+                                            Wind speed 12 km/h
+                                        </li>
+                                        <li>
+                                            Humidity 80%
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+            </div>
+            <footer>
+<a href="https://github.com/jennie350/react-weather-app-v2" target="_blank" rel="noreferrer" >Open source code </a> by Jenny Watts.
+            </footer>
+            </div>
+       
     )
 }
