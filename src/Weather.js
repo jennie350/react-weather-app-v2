2f70797aa2eb969ab8aeef00b6c84b16c7d2826e
Jenny Watts <93653195+jennie350@users.noreply.github.com>
2022-10-04 18:43:29 +0800

    Created react app and installed bootrap

---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Weather(){
+    return (
+        <div className="Wrapper">
+            <h1>Weather app</h1>
+            
+        </div>
+    )
+}
