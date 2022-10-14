0189fe63930e0ce271dbda8ced21a0b6a8da9440
Jenny Watts <93653195+jennie350@users.noreply.github.com>
2022-10-14 19:44:12 +0800

    added useEffect to update forecast when new city searched

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
 
@@ -7,6 +7,12 @@ import axios from "axios";
 export default function WeatherForecast(props) {
     let [loaded, setLoaded]= useState(false);
     let [forecast, setForecast]=useState(null);
+
+    useEffect(() => {
+setLoaded(false);
+    }
+    ,[props.coordinates]
+    );
 
     function showForecast(response){
         setForecast(response.data.daily);
